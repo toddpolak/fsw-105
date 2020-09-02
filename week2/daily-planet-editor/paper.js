f9dcd010dad1eeb949bff1bebc19dd767b5e5325
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-09-01 20:53:19 -0700

    daily-planet-editor

--- week2/daily-planet-editor/paper.js
+++ week2/daily-planet-editor/paper.js
@@ -9,24 +9,21 @@ just fix the existing ones.
 
 // Written by Kent, Clark
 
-var Enemies=["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
+var enemies = ["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
 
-funtion Who_Wins(is-there-kryptonite,enemy_name)
-{
-if (!is-there-kryptonite)
-{
-    return "Superman beats " +enemyName+ ", of course";
-}
-
-else{
-    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyname +" could possibly win this one.";
-}
+function Who_Wins(is_there_kryptonite, enemy_name) {
+    if (!is_there_kryptonite) {
+        return "Superman beats " + enemy_name + ", of course";
     }
+    else {
+        return "Depends on how quick Superman can get rid of the Kryptonite. " + enemy_name + " could possibly win this one.";
+    }
+}
 
 for (var i=0;i<enemies.length;i++)
 {
-    var is_there_kryptonite:
-    if (i%2=== 0) {
+    var is_there_kryptonite;
+    if (i % 2 === 0) {
         is_there_kryptonite = true;
     } else {
         is_there_kryptonite = false;
@@ -37,18 +34,23 @@ for (var i=0;i<enemies.length;i++)
 function HowAttractedIsLoisLaneToMe ()
 {
     // 1 is not at all attracted, 10 is "super" attracted...
-    return Math.floor((Mat.random()*10)+1);
+    return Math.floor((Math.random() * 10) +1);
 }
 
-consol.log( HowAttractedIsLoisLaneToMe ()) ;
+console.log( HowAttractedIsLoisLaneToMe ()) ;
 
-var clarkKent =true;
-var superman false;
+var clarkKent = true;
+var superman = false;
 
 while (clarkKent){
+
     console.log("I'm just a nerdy columnist");
+
     var phoneBoothQuickChange =Math.random();
+
     if (phoneBoothQuickChange>= 0.5) {
         clarkKent=false;
         superman=true;
-        console.log("Now I'm Superman!");}}
+        console.log("Now I'm Superman!");
+    }
+}
