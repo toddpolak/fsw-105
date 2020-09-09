ad45ad8753f314e2e4a23583abee4821ce40d935
unknown <todd.polak@bryanuniversity.edu>
2020-09-08 17:08:50 -0700

    functions

--- week3/functions/script.js
+++ week3/functions/script.js
@@ -27,22 +27,3 @@ function stringManipulation(str) {
 }
 console.log(stringManipulation('I really enjoy learning JavaScript'))
 console.log(stringManipulation('JavaScript'))
-
-//*Extra Credit*
-function fibonacciExample(n) {
-    
-}
-
-/*
-Write a function that accepts a number ‘n’ as a parameter. Then print the first ‘n’ Fibonacci numbers and return their sum.
-
-The first numbers are:
-
-1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
-
-So if n were 6, the sum of 1+1+2+3+5+8 would be 20
-
-Don't hardcode the sequence.
-
-Write a function that accepts a string as a parameter. Return the most frequently occurring letter in that string. (White spaces count as a letter)
-*/
