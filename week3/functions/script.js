157064fa03db4f56e36f7e6e7e42b33c871a53ff
unknown <todd.polak@bryanuniversity.edu>
2020-09-08 20:43:06 -0700

    functions

--- week3/functions/script.js
+++ week3/functions/script.js
@@ -27,3 +27,15 @@ function stringManipulation(str) {
 }
 console.log(stringManipulation('I really enjoy learning JavaScript'))
 console.log(stringManipulation('JavaScript'))
+
+function fibonacci(n) {
+    if (n <= 2) return 1;
+  
+    const f = [0, 1, 1];
+    for(let i = 3; i <= n; i++) {
+      f[i] = f[i-1] + f[i-2];
+    }
+    return f;
+    //return f[f.length-1];
+  }
+  console.log(fibonacci(5))
