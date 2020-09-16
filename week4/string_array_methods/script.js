917da21f3f3f4818f7422af5668ff521eceaec2f
unknown <todd.polak@bryanuniversity.edu>
2020-09-16 10:52:31 -0700

    String and Array Methods

--- week4/string_array_methods/script.js
+++ week4/string_array_methods/script.js
@@ -1,6 +1,34 @@
-var myString = 'Javascript';
+var myString = 'javascript development is fun';
+console.log('Value used: ' + myString);
 
-function capalizeAndLowercase(str) {
+function capitalizeAndLowercase(str) {
     return str.toUpperCase() + str.toLowerCase()
 }
-console.log(capalizeAndLowercase(myString));
+console.log('All caps followed by the same string all lowercase: ' + capitalizeAndLowercase(myString));
+
+function findMiddleIndex(str) {
+    return Math.floor(str.length / 2);
+}
+console.log('Half the length, and rounded down: ' + findMiddleIndex(myString));
+
+function returnFirstHalf(str) {
+    return str.slice(0, Math.floor(str.length / 2));
+}
+console.log('First half of the string: ' + returnFirstHalf(myString));
+
+function capAndLower(str) {
+    var firstHalf = str.slice(0, Math.floor(str.length / 2));
+    var secondHalf = str.slice(str.length / 2);
+
+    return firstHalf.toUpperCase() + secondHalf.toLowerCase();
+}
+console.log('First half capitalized, second half lowercase: ' + capAndLower(myString));
+
+function capitalizeFirstLetter(str) {
+    var strAry = str.toLowerCase().split(' ');
+    for (var i = 0; i < strAry.length; i++) {
+        strAry[i] = strAry[i].charAt(0).toUpperCase() + strAry[i].substring(1);     
+    }
+   return strAry.join(' ');
+}
+console.log('Capitalize any character that follows a space: ' + capitalizeFirstLetter(myString));
