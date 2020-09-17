13e3ad1c1cd264954a9b79a3027e76bb4ba0db99
unknown <todd.polak@bryanuniversity.edu>
2020-09-16 19:15:19 -0700

    String and Array Methods

---
+++ week4/string_array_methods/array_methods.js
@@ -0,0 +1,20 @@
+var fruit = ['bannana', 'apple', 'orange', 'watermelon'];
+var vegetables = ['carrot', 'tomato', 'pepper', 'lettuce'];
+
+function arrayMethods() {
+
+    vegetables.pop();
+
+    fruit.shift();
+    
+    console.log('fruit: ' + fruit);
+
+    console.log('vegetables: ' + vegetables);
+
+    var orangeIndex = fruit.indexOf('orange');
+
+    console.log('orange index: ' + orangeIndex);
+
+}
+
+arrayMethods();
