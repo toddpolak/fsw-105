d7b633e1e0baec75d0e1689267155e2cd53c494e
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-09-16 22:30:53 -0700

    Javascript Calculator

--- week4/javascript_calculator/calculator.js
+++ week4/javascript_calculator/calculator.js
@@ -1,7 +1,11 @@
 const readlineSync = require('readline-sync');
 
-var firstNumber = readlineSync.question('Please enter your first number:');
-var secondNumber = readlineSync.question('Please enter your second number:');
+var firstNumber = readlineSync.question('Please enter your first number: ');
+var secondNumber = readlineSync.question('Please enter your second number: ');
+
+var operation = ['Add', 'Subtract', 'Multiply', 'Divide'],
+  index = readlineSync.keyInSelect(operation, 'Please enter the operation to perform:');
 
 console.log('First Number: ' + firstNumber);
 console.log('Second Number: ' + secondNumber);
+console.log('Operation: ' + index);
