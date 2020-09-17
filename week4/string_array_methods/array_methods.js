373562e5198005068a50c4c6df2b2b0f6bf93601
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-09-16 21:06:26 -0700

    String and Array Methods

--- week4/string_array_methods/array_methods.js
+++ week4/string_array_methods/array_methods.js
@@ -4,25 +4,27 @@ var vegetables = ['carrot', 'tomato', 'pepper', 'lettuce'];
 function arrayMethods() {
 
     vegetables.pop();
+    console.log('vegetables: ' + vegetables);
 
     fruit.shift();
-    
     console.log('fruit: ' + fruit);
 
-    console.log('vegetables: ' + vegetables);
-
-    var orangeIndex = fruit.indexOf('orange');
-
-    console.log('orange index: ' + orangeIndex);
-
-    fruit.push(orangeIndex);
-
+    fruit.push(fruit.indexOf('orange'));
     console.log('fruit: ' + fruit);
 
+    vegetables.push(vegetables.length);
     console.log('vegetables: ' + vegetables);
 
-    
+    var food = fruit.concat(vegetables);
+    console.log('food: ' + food);
 
+    food.splice(4, 2)
+    console.log('food: ' + food);
+
+    food.reverse();
+    console.log('food: ' + food);
+
+    return food.toString();
 }
 
-arrayMethods();
+console.log(arrayMethods());
