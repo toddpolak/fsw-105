63f796f391d763652efac8a578d953d726230931
unknown <todd.polak@bryanuniversity.edu>
2020-09-17 18:47:21 -0700

    Javascript Calculator

--- week4/javascript_calculator/calculator.js
+++ week4/javascript_calculator/calculator.js
@@ -1,24 +1,29 @@
 const readlineSync = require('readline-sync'),
   chalk = require('chalk'),
   methods = require('./methods');
+const questionColor = chalk.magentaBright.bold;
+const answerColor = chalk.yellowBright.bold;;
+let index, operation;
 
-let firstNum = readlineSync.questionInt(chalk.blueBright.bold('Please enter your first number: '));
-let secondNum = readlineSync.questionInt(chalk.greenBright.bold('Please enter your second number: '));
+while (index !== -1) {
+  let firstNum = readlineSync.questionInt(questionColor('Please enter your first number: '));
+  let secondNum = readlineSync.questionInt(questionColor('Please enter your second number: '));
+  
+  operation = ['Add', 'Subtract', 'Multiply', 'Divide'],
+    index = readlineSync.keyInSelect(operation, questionColor('Please enter the operation to perform:'));
 
-let operation = ['Add', 'Subtract', 'Multiply', 'Divide'],
-  index = readlineSync.keyInSelect(operation, chalk.redBright.bold('Please enter the operation to perform:'));
-
-switch (index) {
-  case 0: console.log('The Sum of ' + firstNum + ' and ' + secondNum + ' Results in: ' 
-    + chalk.yellow.bold(methods.calcAdd(firstNum, secondNum)));
-    break;
-  case 1: console.log(firstNum + ' Minus ' + secondNum + ' Results in: '
-    + chalk.yellow.bold(methods.calcSub(firstNum, secondNum)));
-    break;
-  case 2: console.log(firstNum + ' Multiplied by ' + secondNum + ' Results in: '
-    + chalk.yellow.bold(methods.calcMult(firstNum, secondNum)));
-    break;
-  case 3: console.log(firstNum + ' Divided by ' + secondNum + ' Results in: '
-    + chalk.yellow.bold(methods.calcDiv(firstNum, secondNum)));
-    break;
+  switch (index) {
+    case 0: console.log('The Sum of ' + firstNum + ' and ' + secondNum + ' Results in: ' 
+      + answerColor(methods.calcAdd(firstNum, secondNum)));
+      break;
+    case 1: console.log(firstNum + ' Minus ' + secondNum + ' Results in: '
+      + answerColor(methods.calcSub(firstNum, secondNum)));
+      break;
+    case 2: console.log(firstNum + ' Multiplied by ' + secondNum + ' Results in: '
+      + answerColor(methods.calcMult(firstNum, secondNum)));
+      break;
+    case 3: console.log(firstNum + ' Divided by ' + secondNum + ' Results in: '
+      + answerColor(methods.calcDiv(firstNum, secondNum)));
+      break;
+  }
 }
