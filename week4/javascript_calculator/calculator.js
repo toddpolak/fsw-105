ed81cc0499304625278f8cd1a940e166d1333449
unknown <todd.polak@bryanuniversity.edu>
2020-09-17 19:54:32 -0700

    Javascript Calculator

--- week4/javascript_calculator/calculator.js
+++ week4/javascript_calculator/calculator.js
@@ -2,13 +2,13 @@ const readlineSync = require('readline-sync'),
   chalk = require('chalk'),
   methods = require('./methods');
 const questionColor = chalk.magentaBright.bold;
-const answerColor = chalk.yellowBright.bold;;
+const answerColor = chalk.yellowBright.bold;
 let index, operation;
 
 while (index !== -1) {
   let firstNum = readlineSync.questionInt(questionColor('Please enter your first number: '));
   let secondNum = readlineSync.questionInt(questionColor('Please enter your second number: '));
-  
+
   operation = ['Add', 'Subtract', 'Multiply', 'Divide'],
     index = readlineSync.keyInSelect(operation, questionColor('Please enter the operation to perform:'));
 
