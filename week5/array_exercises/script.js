c02a71a1bb9ffa4251e3f6652c15842883dab43b
unknown <todd.polak@bryanuniversity.edu>
2020-09-21 12:20:56 -0700

    Week 5 PnP Higher Order Array Methods

--- week5/array_exercises/script.js
+++ week5/array_exercises/script.js
@@ -7,3 +7,14 @@ function evensOnly(arr) {
     return arr.filter(num => num % 2 === 0);
 }
 console.log(evensOnly([3, 6, 8, 2]));
+
+function ofAge(arr) {
+    return arr.filter(person => person.age >= 17);
+}
+console.log(ofAge([
+    { name: 'Angelina Jolie', age: 80 },
+    { name: 'Eric Jones', age: 2 },
+    { name: 'Paris Hilton', age: 5 },
+    { name: 'Kayne West', age: 16 },
+    { name: 'Bob Ziroll', age: 100 }
+]));
