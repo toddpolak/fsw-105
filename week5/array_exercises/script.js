e492508d84574c9380458abfacdf7d02edb2f705
unknown <todd.polak@bryanuniversity.edu>
2020-09-22 10:49:35 -0700

    PnP Higher Order Array Methods

--- week5/array_exercises/script.js
+++ week5/array_exercises/script.js
@@ -10,64 +10,73 @@ const arrNames = [
 function fiveAndGreaterOnly(arr) {
     return arr.filter(num => num > 5);
 }
-console.log(fiveAndGreaterOnly([3, 6, 8, 2]));
+//console.log(fiveAndGreaterOnly([3, 6, 8, 2]));
 
 function evensOnly(arr) {
     return arr.filter(num => num % 2 === 0);
 }
-console.log(evensOnly([3, 6, 8, 2]));
+//console.log(evensOnly([3, 6, 8, 2]));
 
 function ofAge(arr) {
     return arr.filter(person => person.age >= 17);
 }
-console.log(ofAge(arrNames));
+//console.log(ofAge(arrNames));
 
 // Section B
 function doubleNumbers(arr) {
     return arr.map(number => number * 2)
 }
-console.log(doubleNumbers([2, 5, 100]));
+//console.log(doubleNumbers([2, 5, 100]));
 
 function stringItUp(arr) {
     return arr.map(num => num.toString());
 }
-console.log(stringItUp([2, 5, 100]));
+//console.log(stringItUp([2, 5, 100]));
 
 function capitalizeNames(arr) {
     return arr.map(name => name[0].toUpperCase() + name.slice(1).toLowerCase());
 }
-console.log(capitalizeNames(['john', 'JACOB', 'jinGleHeimer', 'schmidt']));
+//console.log(capitalizeNames(['john', 'JACOB', 'jinGleHeimer', 'schmidt']));
 
 function namesOnly(arr) {
     return arr.map(person => person.name)
 }
-console.log(namesOnly(arrNames));
+//console.log(namesOnly(arrNames));
 
 function makeStrings(arr) {
     return arr.map(person => person.age >= 17 ? `${person.name} can go to the Matrix` 
         : `${person.name} is under age!!`);
 }
-console.log(makeStrings(arrNames));
+//console.log(makeStrings(arrNames));
 
 // Section C
+function total(arr) {
+    return arr.reduce((final, num) => final += num);
+}
+console.log(total([1,2,3]));
+
+function stringConcat(arr) {
+    return arr.reduce((final, num) => final + num.toString());
+}
+console.log(stringConcat([1,2,3]));
 
 // Section D
 function leastToGreatest(arr) {
     return arr.sort((a, b) => a - b);
 }
-console.log(leastToGreatest([1, 3, 5, 2, 90, 20]));
+//console.log(leastToGreatest([1, 3, 5, 2, 90, 20]));
 
 function greatestToLeast(arr) {
     return arr.sort((a, b) => b - a);
 }
-console.log(greatestToLeast([1, 3, 5, 2, 90, 20]));
+//console.log(greatestToLeast([1, 3, 5, 2, 90, 20]));
 
 function lengthSort(arr) {
     return arr.sort((a, b) => a.length - b.length);
 }
-console.log(lengthSort(['dog', 'wolf', 'by', 'family', 'eaten']));
+//console.log(lengthSort(['dog', 'wolf', 'by', 'family', 'eaten']));
 
 function alphabetical(arr) {
     return arr.sort();
 }
-console.log(alphabetical(['dog', 'wolf', 'by', 'family', 'eaten']));
+//console.log(alphabetical(['dog', 'wolf', 'by', 'family', 'eaten']));
