e5459dc34d7ac9b4dc54b524a1906e18c2eb9e77
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-09-21 22:41:21 -0700

    PnP Higher Order Array Methods

--- week5/array_exercises/script.js
+++ week5/array_exercises/script.js
@@ -10,48 +10,43 @@ const arrNames = [
 function fiveAndGreaterOnly(arr) {
     return arr.filter(num => num > 5);
 }
-//console.log(fiveAndGreaterOnly([3, 6, 8, 2]));
+console.log(fiveAndGreaterOnly([3, 6, 8, 2]));
 
 function evensOnly(arr) {
     return arr.filter(num => num % 2 === 0);
 }
-//console.log(evensOnly([3, 6, 8, 2]));
+console.log(evensOnly([3, 6, 8, 2]));
 
 function ofAge(arr) {
-    return arr.filter = function(person) {
-        if (person.age >= 17) {
-            return true;
-        }
-    }
-    //return arr.filter(person => person.age >= 17);
+    return arr.filter(person => person.age >= 17);
 }
-console.log(arrNames);
+console.log(ofAge(arrNames));
 
 // Section B
 function doubleNumbers(arr) {
     return arr.map(number => number * 2)
 }
-//console.log(doubleNumbers([2, 5, 100]));
+console.log(doubleNumbers([2, 5, 100]));
 
 function stringItUp(arr) {
     return arr.map(num => num.toString());
 }
-//console.log(stringItUp([2, 5, 100]));
+console.log(stringItUp([2, 5, 100]));
 
 function capitalizeNames(arr) {
     return arr.map(name => name[0].toUpperCase() + name.slice(1).toLowerCase());
 }
-//console.log(capitalizeNames(['john', 'JACOB', 'jinGleHeimer', 'schmidt']));
+console.log(capitalizeNames(['john', 'JACOB', 'jinGleHeimer', 'schmidt']));
 
 function namesOnly(arr) {
     return arr.map(person => person.name)
 }
-//console.log(namesOnly(arrNames));
+console.log(namesOnly(arrNames));
 
 function makeStrings(arr) {
     return arr.map(person => person.age >= 17 ? `${person.name} can go to the Matrix` 
         : `${person.name} is under age!!`);
 }
-//console.log(makeStrings(arrNames));
+console.log(makeStrings(arrNames));
 
 // Section C
