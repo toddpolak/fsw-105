200f971fa1e23b0de5f58b822f40a6a299087db3
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-09-28 19:39:06 -0700

    Escape Room

--- week6/escape_room/script.js
+++ week6/escape_room/script.js
@@ -1,12 +1,25 @@
 const readlineSync = require('readline-sync'), chalk = require('chalk');
-const defaultColor = chalk.white.bold.underline;
+const defaultColor = chalk.white.bold;
+const cRed = chalk.red.bold;
+const cBlue = chalk.blue.bold;
+const cYellow = chalk.yellow.bold;
+let isAlive = true;
+let hasKey = false;
+let options = `Press ${cYellow(1)} to Put hand in hole\nPress ${cYellow(2)} to Find a key\nPress ${cYellow(3)} to Open the door\nPress ${cYellow(4)} to QUIT\n`;
 
 console.log(chalk.black.bold.bgYellow('     ESCAPE ROOM     '));
 
-const playerName = readlineSync.question(defaultColor('Please enter your name:'));
-
-const colorOptions = ['Black', 'Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan', 'White'];
-
-let playerConsoleColor = readlineSync.keyInSelect(colorOptions, defaultColor('Please select a color for your console:'), {cancel: 'Quit'});
+const playerName = readlineSync.question(defaultColor('Please enter your name: '));
 
 console.log(defaultColor(`Hello ${playerName}, welcome to the Escape Room Simulation!!`));
+console.log(`You are current locked in a room. You need to find a way out!`);
+
+let operation = readlineSync.keyIn(options, {limit: '$<1-4>'});
+
+while (isAlive == true) {
+    switch (options) {
+        case 1:
+                
+            break;
+    }
+}
