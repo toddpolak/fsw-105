135a899bedc457b719ebf3e16588022669e7d5a9
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-09-29 11:11:05 -0700

    Escape Room

--- week6/escape_room/script.js
+++ week6/escape_room/script.js
@@ -1,20 +1,24 @@
 const readlineSync = require('readline-sync'), chalk = require('chalk');
-const defaultColor = chalk.white.bold;
+const cWhite = chalk.white.bold;
 const cRed = chalk.red.bold;
-const cBlue = chalk.blue.bold;
 const cYellow = chalk.yellow.bold;
+const cBlue = chalk.cyanBright.bold;
+const cMagenta = chalk.magentaBright.bold;
+const cBox = chalk.black.bold.bgYellow;
 let isAlive = true;
 let hasKey = false;
 let escapedRoom = false;
-let options = `Press ${cYellow(1)} to Put hand in hole\nPress ${cYellow(2)} to Find a key\nPress ${cYellow(3)} to Open the door\nPress ${cYellow(4)} to QUIT\n`;
 let operation;
+let options = 'Here are your options:\nPress ' + cRed('1') + ' to Put your hand in the ' + cRed('hole') + '\nPress ' 
+    + cYellow('2') + ' to Find a ' + cYellow('key') + '\nPress ' + cBlue('3') 
+    + ' to Open the ' + cBlue('door') + '\nPress ' + cWhite('4') + ' to ' + cWhite('QUIT') +'\n';
 
-console.log(chalk.black.bold.bgYellow('     ESCAPE ROOM     '));
+console.log(cBox(`     ESCAPE ROOM     `));
 
-const playerName = readlineSync.question(defaultColor(`Please enter your name: `));
+const playerName = readlineSync.question(`${cWhite('Please enter your name: ')}`);
 
-console.log(defaultColor(`Hello ${cRed(playerName)}, welcome to the Escape Room Simulation!!`));
-console.log(`You are currently locked in a room. You need to find a way out!`);
+console.log(cWhite(`\nHello ${cMagenta(playerName)}, welcome to the Escape Room Simulation!!`));
+console.log(`\nYou are currently ${cYellow('locked')} in a room. There is a small ${cRed('hole')} in the wall. You need to find a ${cBlue('way out!')}\n`);
 
 while (isAlive == true) {
     if (escapedRoom == true || operation == '4') { break; }
@@ -23,22 +27,22 @@ while (isAlive == true) {
 
     switch (operation) {
         case '1': 
-            console.log(`Putting your hand in a hole... You DIED!!`);
+            console.log(`\nPutting your hand in the ${cRed('hole')}... You ${cRed('DIED!!')}`);
             isAlive = false;
             break;
         case '2':
             if (hasKey == false) {
-                console.log(`Searching the room... You found a key!`);
+                console.log(`\nSearching the room... You found a ${cYellow('key!')}!\n`);
                 hasKey = true;
             } else {
-                console.log(`You already found a key.`)
+                console.log(`\nYou already found a ${cYellow('key')}.\n`)
             }
             break;
         case '3':
             if (hasKey == false) {
-                console.log(`In order to open the door, you must have a key!`);
+                console.log(`\nIn order to open the ${cBlue('door')}, you must have a ${cYellow('key')}!\n`);
             } else {
-                console.log(`Congradulations!! You found your way out of the room.`);
+                console.log(`\n${cBlue('Congradulations!!')} You found your way out of the ${cBox('ROOM!')}`);
                 escapedRoom = true;
             }
     }
