4f896dbe3a8e004645c006e795808c53c84fc933
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-10-02 18:20:33 -0700

    Console RPG

--- week7/console_rpg/rpg.js
+++ week7/console_rpg/rpg.js
@@ -1,2 +1,3 @@
 const readlineSync = require('readline-sync'), chalk = require('chalk');
 
+console.log(chalk.bgCyanBright.yellowBright.bold.italic(`          WELCOME TO A FANTASY ROLE PLAYING GAME          `));
