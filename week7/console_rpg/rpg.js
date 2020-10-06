f95404c4aa1f05f942f7d9d427e1d78f54a96481
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-10-05 23:19:39 -0700

    Console RPG

--- week7/console_rpg/rpg.js
+++ week7/console_rpg/rpg.js
@@ -6,7 +6,7 @@ const cYellow = chalk.yellow.bold;
 const cBlue = chalk.cyanBright.bold;
 const cMagenta = chalk.magentaBright.bold;
 const cBox = chalk.black.bold.bgYellow;
-const playerOptions = 'Enter w to walk, l to look around, or (q to quit)';
+const playerOptions = 'Enter w to walk, l to look around, or (q to quit) ';
 
 console.log(chalk.bgCyanBright.yellowBright(`                                                    `));
 console.log(chalk.bgCyanBright.yellowBright.bold(`             A FANTASY ROLE PLAYING GAME            `));
@@ -16,7 +16,7 @@ const playerName = readlineSync.question(`${cWhite('Please enter your name: ')}`
 
 console.log(cWhite(`\nHello ${cMagenta(playerName)}, welcome to a Fantasy Role Playing Game!!`));
 
-//let playerAction = readlineSync.keyIn(`${cWhite(playerOptions), {limit: 'wlq'}}`);
+let playerAction = readlineSync.keyIn(`${cWhite(playerOptions)}`, {limit: 'wlq'});
 
 let enemyAppears = commonMethods.enemyAppears();
 
