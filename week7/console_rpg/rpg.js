46084c5dc4b91ea3d52d0112c9b5e088f1e2326f
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-10-08 12:58:49 -0700

    Console RPG

--- week7/console_rpg/rpg.js
+++ week7/console_rpg/rpg.js
@@ -8,21 +8,24 @@ const cBlue = chalk.cyanBright.bold;
 const cMagenta = chalk.magentaBright.bold;
 const cBox = chalk.black.bold.bgYellow;
 const playerOptions = ['Walk', 'Print', 'Quit'];
-//const playerOptions = 'What would you like to do? Enter w to walk, l to look around, Print, or (q to quit) ';
-//let isAlive = true;
 
-//console.log(chalk.bgCyanBright.yellowBright(`                                                    `));
-//console.log(chalk.bgCyanBright.yellowBright.bold(`             A FANTASY ROLE PLAYING GAME            `));
-//console.log(chalk.bgCyanBright.yellowBright(`                                                    `));
+console.log(chalk.bgCyanBright.yellowBright(`                                                    `));
+console.log(chalk.bgCyanBright.yellowBright.bold(`             A FANTASY ROLE PLAYING GAME            `));
+console.log(chalk.bgCyanBright.yellowBright(`                                                    `));
 
-const playerName = readlineSync.question(`${cWhite('Please enter your name: ')}`);
+const playerName = readlineSync.question(`${cWhite('Please enter your name to begin: ')}`);
 
 let playerInventory = new commonMethods(playerName);
+
+console.log(cWhite(`\nHello ${cMagenta(playerName)}, welcome to a Fantasy Role Playing Game!!`));
+
+//const playerOptions = 'What would you like to do? Enter w to walk, l to look around, Print, or (q to quit) ';
+//let isAlive = true;
 
 // Testing
 //playerInventory.hp = 5;
 
-console.log(cWhite(`\nHello ${cMagenta(playerName)}, welcome to a Fantasy Role Playing Game!!`));
+//console.log(playerAction.toLowerCase());
 
 //console.log(cWhite(`At any time during the game, type: 'Print' to see your inventory.`));
 
@@ -31,8 +34,6 @@ console.log(cWhite(`\nHello ${cMagenta(playerName)}, welcome to a Fantasy Role P
 //let playerAction = readlineSync.question(playerOptions);
 //let playerAction = readlineSync.prompt(playerOptions);
 //let playerAction = readlineSync.prompt(`${cWhite(playerOptions)}`, {limit: 'wlq'});
-
-console.log(playerAction.toLowerCase());
 
 /*
 while (isAlive) {
